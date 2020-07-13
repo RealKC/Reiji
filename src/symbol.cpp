dffafd19085d40d51cfbbff3a44d147f35ba93b1
[RealKC] <16511305+RealKC@users.noreply.github.com>
2020-07-14 02:55:56 +0300

    Make symbol_base::remov_self... more correct

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -11,12 +11,22 @@
 
 namespace reiji::detail {
 
-void symbol_base::remove_self() noexcept {
+void symbol_base::remove_self_from_origins_symbol_vector() noexcept {
+    if (not _origin) {
+        return;
+    }
+
+    // Let's make sure none of the removed symbols exist in the _symbols vector
+    // makes our life easier
+    _origin->_symbols.erase(
+        std::remove(_symbols.begin(), _symbols.end(), nullptr), _symbols.end());
+
     // We can binary search to find ourselves as we always push to the back of
     // the _symbols vector in unique_shared_lib
     std::size_t low = 0, high = _origin->_symbols.size() - 1, mid;
     while (low <= high) {
         mid = (low + high) / 2;
+
         if (_origin->_symbols[mid]->_uid > _uid) {
             high = mid - 1;
         } else if (_origin->_symbols[mid]->_uid < _uid) {
