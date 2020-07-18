5f07260f2c6205618337ad08d62f649a967f9fce
[RealKC] <16511305+RealKC@users.noreply.github.com>
2020-07-18 22:41:26 +0300

    Try and fix CI again
    also remove some unneeded stuff from the tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
-#include <filesystem>
 #include <string>
-
-std::string path_to_my_directory;
-
-namespace fs = std::filesystem;
 
 int main(int argc, char** argv) {
     doctest::Context ctx;
